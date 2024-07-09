0f0f694bfc1350208e4d4d4feaa4fae37281bc68
DESKTOP-DSP8N0I\eniste <123>
2024-07-09 16:30:28 +0300

    no message

--- proje1/Controllers/KategoriController.cs
+++ proje1/Controllers/KategoriController.cs
@@ -38,5 +38,35 @@ namespace proje1.Controllers
             }
             return View(gelenData);
         }
+
+        public IActionResult kategoriDuzenle(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Kategori? dbDatasi = _veritabani.Kategoriler.Find(id);
+            /*Kategori? dbDatasi = _veritabani.Kategoriler.Find(x => x.Id == id);*/
+
+            if (dbDatasi == null)
+            {
+                return NotFound();
+            }
+            return View(dbDatasi);
+        }
+
+
+
+        [HttpPost]
+        public IActionResult kategoriDuzenle(Kategori gelenData)
+        {
+            if (ModelState.IsValid)
+            {
+                _veritabani.Kategoriler.Add(gelenData);
+                _veritabani.SaveChanges();
+                return RedirectToAction("Index", "Kategori");
+            }
+            return View(gelenData);
+        }
     }
 }
